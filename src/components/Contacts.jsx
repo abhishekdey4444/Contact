3d293b6d3702ab0572fa2208fd3a5a30e6e0090e
abhishekdey4444 <abhishekdey4444@gmail.com>
2021-04-20 23:34:47 +0530

    latest

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,23 +4,23 @@ const contacts=[{
   Tel: "5522533353"
 },
 {
-  Name:"haddi",
-  Image: "",
+  Name:"HADDI",
+  Image: "https://static.wikia.nocookie.net/naruto/images/2/27/Kakashi_Hatake.png/revision/latest?cb=20170628120149",
   Tel: "5522533353"
 },
 {
-  Name:"oyo",
-  Image: "",
+  Name:"OYO",
+  Image: "https://cdn.discordapp.com/attachments/797544942234632263/834123092737392650/c05e93a73807574d33d80a86e9b129a8-donkey-yelling-stroke-by-vexels.png",
   Tel: "5522533353"
 },
 {
-  Name:"deathshot",
-  Image: "",
+  Name:"Deathshot110",
+  Image: "https://static.wikia.nocookie.net/naruto/images/2/27/Kakashi_Hatake.png/revision/latest?cb=20170628120149",
   Tel: "5522533353"
 },
 {
-  Name:"immortal",
-  Image: "",
+  Name:"!mm0rtal",
+  Image: "https://static.wikia.nocookie.net/naruto/images/2/27/Kakashi_Hatake.png/revision/latest?cb=20170628120149",
   Tel: "5522533353"
 }]
 
--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -3,23 +3,23 @@ import contacts from "./Contacts";
 
 
 function Contents(){
-  return <div>
+  return <div class="grid">
 
-
+  {contacts.map((item,index)=>(
   <div class="box">
   <div class="banner">
   <img
-  src={contacts[0].Image}
+  src={item.Image}
   alt="NA"/>
   </div>
   <div class="details">
-  <h6>NAME : {contacts[0].Name}</h6>
-  <h6>Tel : {contacts[0].Tel}</h6>
+  <h6>NAME : {item.Name}</h6>
+  <h6>Tel : {item.Tel}</h6>
   </div>
   </div>
+))}
 
-
-  </div>
+</div>
 }
 
 export default Contents;
